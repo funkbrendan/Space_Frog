42fc5121f14f5a7042d9c575accce26368e9aa60
Micah Price-Offermann <micah.offerman@gmail.com>
2021-01-25 20:13:18 -0600

    Fixed basic movement script as well as added dash mech script
    Cleaned up and fixed the Basic Movement script and attached it to the PlayerCharacter sprite. Added Dash_Mech script but haven't gotten it to work yet. Need to research how to call the public variables generated in the Basic Movement script

---
+++ Assets/Dash_Mech.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Dash_Mech : MonoBehaviour
+{
+    public float dashDistance = 25f;        //Distance needs tuned
+    public bool dashKeyWasPressed;          //Dash key is space
+    public class BasicMovement: MonoBehaviour; //Need to figure out how to call this script to access these variables.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Debug.LogFormat("I pressed space, you son of a bitch");
+            dashKeyWasPressed = true;
+        }
+    }
+
+    void FixedUpdate()  // Fixed update is called at a regular interval, unless specified, 50 fps for desktop computers
+    {
+        if (dashKeyWasPressed)
+        {
+            positionChange.x = Input.GetAxisRaw("Horizontal")*dashDistance;
+            positionChange.y = Input.GetAxisRaw("Vertical")*dashDistance;
+            dashKeyWasPressed = false;
+        }
+    }
+}
--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -7,8 +7,8 @@ using UnityEngine;
 public class BasicMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;            //Movement speed needs tuned 
-    Rigidbody2D PC;     //Player character
-    public Vector2 positionChange;                       //= transform.position;  -- no need to transform the position of the sprite since we will actually be modifying the position of the player character with the RigidBody2D component
+    public Rigidbody2D PC; //= 0;           //Player character
+    public Vector2 positionChange;          //= transform.position;  -- no need to transform the position of the sprite since we will actually be modifying the position of the player character with the RigidBody2D component
 
     // Start is called before the first frame update
     void Start()
@@ -22,15 +22,11 @@ public class BasicMovement : MonoBehaviour
         positionChange.y = Input.GetAxis("Vertical");
         positionChange.x = Input.GetAxis("Horizontal");
 
-        //code graveyard
-        //transform.position = position; -- couldn't figure out why, but this gave me a blue screen for some reason?
-
     }
 
     // Fixed update is called at a regular interval, unless specified, 50 fps for desktop computers
     void FixedUpdate()
     {
-        /*position.x*/PC.position = PC.position + positionChange * /*3.0f*/moveSpeed /** horizontal*/ * Time.deltaTime; //having move speed as a public variable means that we can modify it in other scripts and make the movement a little more interactive
-      //  /*position.y*/PlayerCharacter.position = position.y + /*3.0f*/ moveSpeed * vertical * Time.deltaTime; //only need one since the rigidbody2d class has both an x and a y and so does our position vector
+       PC.position = PC.position + positionChange * moveSpeed * Time.deltaTime; //having move speed as a public variable means that we can modify it in other scripts and make the movement a little more interactive
     }
 }
